95ccf2fa642bcc0d1416ed76b24e93d2bfa4756a
Chu Chinh <7a3dqh@gmail.com>
2022-01-10 13:07:32 +0700

    fist

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  background: linear-gradient(52.87deg, #9D3EF2 40.29%, #3036F4 97.93%);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="timeline">
+      <div v-for="(value,i) in dataRoadMap"  :key="i" :class="value.active?'active-con':null">
+        <div  class="container" :key="i" :class="i%2===0?'left':'right'">
+          <div class="content" :class="value.active?'active':null">
+            <h2>{{ value.title1 }}</h2>
+            <p>
+              {{value.title2 }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      dataRoadMap:[
+        {
+          title1:"NOV 2021",
+          title2:"Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.",
+          active:true
+        },
+        {
+          title1:"NOV 2021",
+          title2:"Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.",
+          active:false
+        },
+        {
+          title1:"NOV 2021",
+          title2:"Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.",
+          active:false
+        },
+        {
+          title1:"NOV 2021",
+          title2:"Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.",
+          active:false
+        },
+        {
+          title1:"NOV 2021",
+          title2:"Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.",
+          active:false
+        }, {
+          title1:"NOV 2021",
+          title2:"Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.",
+          active:false
+        }, {
+          title1:"NOV 2021",
+          title2:"Lorem ipsum dolor sit amet elit. Aliquam odio dolor, id luctus erat sagittis non. Ut blandit semper pretium.",
+          active:false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="css" src="@/components/style.css">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
